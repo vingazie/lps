3836296d32b2da0b8f0ba699b8195950a145812c
pasquale@zirpoli.net <pasquale@zirpoli.net>
2018-01-28 12:19:09 +0100

    commit scrauso

---
+++ cosescrause/comeinfatti.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pasquale
+ * Date: 28/01/2018
+ * Time: 12:16
+ */
+
+
+echo 'Don Gaetano';
---
+++ cosescrause/infinitependigcigarettecinder.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pasquale
+ * Date: 28/01/2018
+ * Time: 12:18
+ */
+
+
+echo 'mario il barbiere can handle it';
